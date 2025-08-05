cdb1e8667f0499c1a37adb7490c415028fba3600
Gaetano Zangari <gaetano.zangari@intesys.it>
2025-08-05 11:59:09 +0200

    feat: Logout Button

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,10 +1,18 @@
 import classes from './Actions.module.css';
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
+import  keycloak  from './keycloak'; //
 
 export function Actions() {
   const location = useLocation();
   const isWorkloadPage = location.pathname.startsWith('/workload');
+
+  const handleLogout = () => {
+    keycloak.logout({
+      redirectUri: window.location.origin 
+    });
+  };
+
   return (
     <div className={classes.actions}>
       <ul>
@@ -14,6 +22,9 @@ export function Actions() {
           ) : (
             <Link to="/projects/add" className={classes.link}>Nuovo Progetto</Link>
           )}
+        </li>
+        <li>
+          <button onClick={handleLogout} className={classes.link}>Logout</button>
         </li>
       </ul>
     </div>
